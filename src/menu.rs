98cc83417287981c90fd3ad464ae2c2edac07871
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-26 23:44:15 +0300

    rework for new commands

--- src/menu.rs
+++ src/menu.rs
@@ -1,6 +1,7 @@
 use crate::console;
 use crate::generate;
 use crate::models;
+use crate::command;
 
 pub fn menu_print() {
     println!("_____ _ _ _ ____  _____");
@@ -8,8 +9,8 @@ pub fn menu_print() {
     println!("|   __| | | |  |  | | | |");
     println!("|__|  |_____|____/|_|_|_|");
     loop {
-        match console::read::<String>("> ").as_str() {
-            "/gen" => {
+        match command::Command::from(console::read::<String>("> ").as_str()) {
+            command::Command::Generate => {
                 let service = models::Service {
                     service_name:console::read::<String>("Service name: "),
                     login:console::read::<String>("Login: "),
@@ -17,12 +18,18 @@ pub fn menu_print() {
                 };
                 println!("{}",service);
             }
-            "/help" => {println!("/gen - generate new password\n/help - display this help\n/exit - exit");}
-            "/exit" => return,
-            _=> {
-                println!("Unknown command. (use /help to see available commands)");
+            command::Command::Help => {print_help();}
+            command::Command::Quit => return,
+            command::Command::Unknown(command) => {
+                eprintln!("Unknown command '{}'. (use /help to see available commands)", command);
                 continue;
             }
         }
     }
+}
+
+fn print_help() {
+    println!("/help | h - display this help");
+    println!("/gen | generate | g - [SERVICE NAME] [LOGIN] [PASSWORD LENGTH] - generate random password");
+    println!("/quit | q - exit");
 }
