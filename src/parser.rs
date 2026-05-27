605bc589f160e7d94a78d3dbe4ad1a7a5bc4853a
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-27 20:57:51 +0300

    New function: generates a random password by getting the input length (usize) Code reforming (use crate::...)

--- src/parser.rs
+++ src/parser.rs
@@ -1,29 +1,43 @@
-use crate::errors;
-use crate::models;
-use crate::generate;
+use crate::errors::{AppError,ErrorType};
+use crate::models::Service;
+use crate::generate::generate_password;
 
-pub fn parse_generate(iter: &mut std::str::SplitWhitespace) -> Result<models::Service, errors::AppError> {
-    let mut result: models::Service = models::Service { service_name: String::new(), login: String::new(), password: String::new() };
+pub fn parse_generate(iter: &mut std::str::SplitWhitespace) -> Result<Service, AppError> {
+    let mut result: Service = Service {
+        service_name: String::new(),
+        login: String::new(),
+        password: String::new()
+    };
+
     match iter.next() {
         Some(name) => { result.service_name = name.to_string(); },
-        None => return Err(errors::AppError { err_type: errors::ErrorType::IncorrectNumberOfParameters, message: String::from("Can't find service name.") })
+        None => return Err(AppError::new(ErrorType::IncorrectNumberOfParameters, String::from("Can't find service name.")))
     }
+
     match iter.next() {
         Some(login) => { result.login = login.to_string(); },
-        None => return Err(errors::AppError { err_type: errors::ErrorType::IncorrectNumberOfParameters, message: String::from("Can't find login.") })
+        None => return Err(AppError::new(ErrorType::IncorrectNumberOfParameters, String::from("Can't find login.")))
     }
     match iter.next() {
         Some(password_length) => {
             match password_length.parse::<usize>() {
                 Ok(password_length) => {
                     if password_length >= 8 {
-                        result.password = generate::generate_password(password_length);
-                    } else { return Err(errors::AppError { err_type: errors::ErrorType::IncorrectPasswordLength, message: String::from("Password is too short.") }) }
+                        result.password = generate_password(password_length);
+                    } else { return Err(AppError::new(ErrorType::IncorrectNumberOfParameters, String::from("Can't generate password."))); }
                 }
-                Err(_) => return Err(errors::AppError { err_type: errors::ErrorType::ParseError, message: String::from("Can't parse password in usize.") })
+                Err(_) => return Err(AppError::new(ErrorType::ParseError, String::from("Can't parse password length in usize.")))
             }
         }
-        None => return Err(errors::AppError { err_type: errors::ErrorType::IncorrectNumberOfParameters, message: String::from("Can't find password.") })
+        None => return Err(AppError::new(ErrorType::IncorrectNumberOfParameters,String::from("Can't find password.")))
     }
     Ok(result)
+}
+
+pub fn parse_fast_generate(iter: &mut std::str::SplitWhitespace) -> Result<String, AppError> {
+    let length = iter.next().ok_or_else(||{return AppError::new(ErrorType::IncorrectNumberOfParameters, String::from("Can't find password length."))});
+    match length?.parse::<usize>() {
+        Ok(length) => {Ok(generate_password(length))},
+        Err(_) => Err(AppError::new(ErrorType::ParseError,String::from("Can't parse password length in usize.")))
+    }
 }
