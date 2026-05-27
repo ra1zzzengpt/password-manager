ece36a33fd201bc624ae5d9ad3dfb2531b37dd71
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-27 21:05:49 +0300

    Fast fix: fixed critical error with ok_or_else()

--- src/parser.rs
+++ src/parser.rs
@@ -35,7 +35,7 @@ pub fn parse_generate(iter: &mut std::str::SplitWhitespace) -> Result<Service, A
 }
 
 pub fn parse_fast_generate(iter: &mut std::str::SplitWhitespace) -> Result<String, AppError> {
-    let length = iter.next().ok_or_else(||{return AppError::new(ErrorType::IncorrectNumberOfParameters, String::from("Can't find password length."))});
+    let length = iter.next().ok_or_else(|| AppError::new(ErrorType::IncorrectNumberOfParameters, String::from("Can't find password length.")));
     match length?.parse::<usize>() {
         Ok(length) => {Ok(generate_password(length))},
         Err(_) => Err(AppError::new(ErrorType::ParseError,String::from("Can't parse password length in usize.")))
