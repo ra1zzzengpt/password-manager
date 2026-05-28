0ac50818381f94087a44417d2277cc1ef819e41a
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-28 23:36:35 +0300

    Rework: now it has parse generic function

--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,6 @@
+use crate::errors::{AppError, ErrorType};
+use crate::generate::generate_password;
+
 pub struct Service {
     pub(crate) service_name: String,
     pub(crate) login: String,
@@ -5,11 +8,75 @@ pub struct Service {
 }
 
 impl std::fmt::Display for Service {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(
-            f,
-            "https://{} - {} : {}",
+            formatter,
+            "{} {} {}",
             self.service_name, self.login, self.password
         )
+    }
+}
+
+pub trait PasswordSource {
+    fn get_password(self) -> String;
+}
+
+impl PasswordSource for usize {
+    fn get_password(self) -> String {
+        generate_password(self)
+    }
+}
+impl PasswordSource for String {
+    fn get_password(self) -> String {
+        self
+    }
+}
+
+impl Service {
+    pub fn parse<T>(iter: &mut std::str::SplitWhitespace) -> Result<Service, AppError>
+    where
+        T: std::str::FromStr + PasswordSource,
+    {
+        let service_name = match iter.next() {
+            Some(name) => name.to_string(),
+            None => {
+                return Err(AppError::new(
+                    ErrorType::IncorrectNumberOfParameters,
+                    String::from("Can't find service name."),
+                ));
+            }
+        };
+
+        let login = match iter.next() {
+            Some(login) => login.to_string(),
+            None => {
+                return Err(AppError::new(
+                    ErrorType::IncorrectNumberOfParameters,
+                    String::from("Can't find login."),
+                ));
+            }
+        };
+        let password = match iter.next() {
+            Some(password_length) => match password_length.parse::<T>() {
+                Ok(password_info) => password_info.get_password(),
+                Err(_) => {
+                    return Err(AppError::new(
+                        ErrorType::ParseError,
+                        String::from("Can't parse password length in usize."),
+                    ));
+                }
+            },
+            None => {
+                return Err(AppError::new(
+                    ErrorType::IncorrectNumberOfParameters,
+                    String::from("Can't find password."),
+                ));
+            }
+        };
+        Ok(Service {
+            service_name,
+            login,
+            password,
+        })
     }
 }
