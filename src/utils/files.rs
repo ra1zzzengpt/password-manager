d5b05c83f050143d19b671983b74e20919ed76db
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-29 23:50:58 +0300

    Reworked: rework function

--- src/utils/files.rs
+++ src/utils/files.rs
@@ -1,9 +1,34 @@
-use crate::errors::{AppError, ErrorType};
-use crate::models::Service;
-use std::fs::OpenOptions;
+use crate::models::errors::{AppError, ErrorType};
+use crate::models::service::Service;
+use crate::parser::service::parse_service;
+use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader, Write};
 
-pub fn write_to_file(path: &std::path::Path, content: &str) -> Result<(), AppError> {
+pub fn rewrite_to_file(path: &std::path::Path, content: &Vec<Service>) -> Result<(), AppError> {
+    let mut file = match File::create(path) {
+        Ok(f) => f,
+        Err(_) => {
+            return Err(AppError::new(
+                ErrorType::FileCreateError,
+                String::from("Cannot create file."),
+            ));
+        }
+    };
+    for service in content {
+        match writeln!(file, "{}", service) {
+            Ok(_) => (),
+            Err(_) => {
+                return Err(AppError::new(
+                    ErrorType::FileWriteError,
+                    String::from("Cannot write content."),
+                ));
+            }
+        }
+    }
+    Ok(())
+}
+
+pub fn add_to_file(path: &std::path::Path, content: &str) -> Result<(), AppError> {
     let mut file = match OpenOptions::new().append(true).create(true).open(path) {
         Ok(file) => file,
         Err(_) => {
@@ -37,7 +62,7 @@ pub fn read_from_file(path: &std::path::Path) -> Result<Vec<Service>, AppError> 
     for line in reader.lines() {
         match line {
             Ok(line) => {
-                let parsed_service = match Service::parse::<String>(&mut line.split_whitespace()) {
+                let parsed_service = match parse_service::<String>(&mut line.split_whitespace()) {
                     Ok(service) => service,
                     Err(_) => {
                         return Err(AppError::new(
