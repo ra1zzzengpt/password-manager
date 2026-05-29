5ac9e7af4799e9ef91aefd1f0b528767e3e4e6c8
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-29 23:39:37 +0300

    New module: for parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,18 +1,2 @@
-use crate::errors::{AppError, ErrorType};
-use crate::generate::generate_password;
-
-pub fn parse_fast_generate(iter: &mut std::str::SplitWhitespace) -> Result<String, AppError> {
-    let length = iter.next().ok_or_else(|| {
-        AppError::new(
-            ErrorType::IncorrectNumberOfParameters,
-            String::from("Can't find password length."),
-        )
-    });
-    match length?.parse::<usize>() {
-        Ok(length) => Ok(generate_password(length)),
-        Err(_) => Err(AppError::new(
-            ErrorType::ParseError,
-            String::from("Can't parse password length in usize."),
-        )),
-    }
-}
+pub mod generate;
+pub mod service;
