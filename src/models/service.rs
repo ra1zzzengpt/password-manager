6002ce785f65da7cc3419fd996b6f385a4e8d078
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-29 23:45:11 +0300

    Reworked: service struct reworked

---
+++ src/models/service.rs
@@ -0,0 +1,26 @@
+#[derive(Clone)]
+pub struct Service {
+    service_name: String,
+    login: String,
+    password: String,
+}
+
+impl std::fmt::Display for Service {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            formatter,
+            "{} {} {}",
+            self.service_name, self.login, self.password
+        )
+    }
+}
+
+impl Service {
+    pub fn new(service_name: String, login: String, password: String) -> Service {
+        Service {
+            service_name,
+            login,
+            password,
+        }
+    }
+}
