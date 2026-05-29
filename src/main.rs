45e1a1e594d7d586924ffb2212801148d4ecc301
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-05-29 23:52:01 +0300

    Reworked: modules rework

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-pub mod command;
-pub mod console;
-pub mod container;
-pub mod errors;
-pub mod files;
-pub mod generate;
 pub mod menu;
 pub mod models;
 pub mod parser;
+pub mod utils;
 
 fn main() -> std::io::Result<()> {
     menu::menu_print();
