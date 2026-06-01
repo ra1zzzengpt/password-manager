70795bd8a9b5f0f904c5db0efa491d4b405b18a6
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-06-01 23:21:32 +0300

    Update: parse_param -> next_optional/next_required

--- src/models/command.rs
+++ src/models/command.rs
@@ -1,6 +1,6 @@
 use crate::models::errors::AppError;
 use crate::models::service::Service;
-use crate::parser::param::parse_param;
+use crate::parser::param::{next_optional, next_required};
 use crate::parser::service::parse_service;
 
 pub enum Command {
@@ -26,17 +26,26 @@ impl From<&str> for Command {
         let mut split = command.split_whitespace();
         match split.next().unwrap_or("?") {
             "/h" | "/help" => Command::Help,
+
             "/q" | "/quit" | "/exit" => Command::Quit,
+
             "/g" | "/gen" | "/generate" => Command::Generate(parse_service::<usize>(&mut split)),
-            "/r" | "/rm" | "/remove" => Command::Remove(parse_param::<String>(&mut split)),
+
+            "/r" | "/rm" | "/remove" => Command::Remove(next_required::<String>(&mut split)),
+
             "/a" | "/add" => Command::Add(parse_service::<String>(&mut split)),
-            "/fg" | "/fastgen" => Command::FastGenerate(parse_param::<usize>(&mut split)),
-            "/l" | "/list" => Command::List(match split.next() {
-                Some(service_name) => SubCommand::ByService(service_name.to_string()),
-                None => SubCommand::Default,
+
+            "/fg" | "/fastgen" => Command::FastGenerate(next_required::<usize>(&mut split)),
+
+            "/l" | "/list" => Command::List(match next_optional::<String>(&mut split) {
+                Ok(Some(service_name)) => SubCommand::ByService(service_name),
+                _ => SubCommand::Default,
             }),
+
             "/c" | "/clear" => Command::Clear,
+
             "/s" | "/save" => Command::SaveWithRewrite,
+
             other => Command::Unknown(other.to_string()),
         }
     }
