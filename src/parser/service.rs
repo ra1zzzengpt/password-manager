8b0f80ba214a576cfb23a99656da946c6b5448be
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-06-01 23:28:12 +0300

    Rework: to parse required

--- src/parser/service.rs
+++ src/parser/service.rs
@@ -1,5 +1,6 @@
-use crate::models::errors::{AppError, ErrorType};
+use crate::models::errors::AppError;
 use crate::models::service::Service;
+use crate::parser::param::next_required;
 use crate::utils::generate::generate_password;
 
 pub trait PasswordSource {
@@ -21,42 +22,11 @@ pub fn parse_service<T>(iter: &mut std::str::SplitWhitespace) -> Result<Service,
 where
     T: std::str::FromStr + PasswordSource,
 {
-    let service_name = iter
-        .next()
-        .ok_or_else(|| {
-            AppError::new(
-                ErrorType::IncorrectNumberOfParameters,
-                String::from("Can't find service name."),
-            )
-        })?
-        .to_string();
+    let service_name = next_required::<String>(iter)?;
 
-    let login = iter
-        .next()
-        .ok_or_else(|| {
-            AppError::new(
-                ErrorType::IncorrectNumberOfParameters,
-                String::from("Can't find login."),
-            )
-        })?
-        .to_string();
+    let login = next_required::<String>(iter)?;
 
-    let password = iter
-        .next()
-        .ok_or_else(|| {
-            AppError::new(
-                ErrorType::IncorrectNumberOfParameters,
-                String::from("Can't find password."),
-            )
-        })?
-        .parse::<T>()
-        .map_err(|_| {
-            AppError::new(
-                ErrorType::ParseError,
-                String::from("Can't parse password length in usize."),
-            )
-        })?
-        .get_password();
+    let password = next_required::<T>(iter)?.get_password();
 
     Ok(Service::new(service_name, login, password))
 }
