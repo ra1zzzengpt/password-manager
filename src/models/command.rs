9cfdef9b3fb9782afd54ce2cb63d363a5de97d2d
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-06-02 20:47:56 +0300

    Rework: list and remove command now use target and parse them

--- src/models/command.rs
+++ src/models/command.rs
@@ -1,5 +1,6 @@
 use crate::models::errors::AppError;
 use crate::models::service::Service;
+use crate::models::target::Target;
 use crate::parser::param::{next_optional, next_required};
 use crate::parser::service::parse_service;
 
@@ -7,18 +8,13 @@ pub enum Command {
     Generate(Result<Service, AppError>),
     Add(Result<Service, AppError>),
     FastGenerate(Result<usize, AppError>),
-    Remove(Result<String, AppError>),
+    Remove(Result<Target, AppError>),
     SaveWithRewrite,
     Clear,
-    List(SubCommand),
+    List(Result<Target, AppError>),
     Help,
     Quit,
     Unknown(String),
-}
-
-pub enum SubCommand {
-    Default,
-    ByService(String),
 }
 
 impl From<&str> for Command {
@@ -31,15 +27,32 @@ impl From<&str> for Command {
 
             "/g" | "/gen" | "/generate" => Command::Generate(parse_service::<usize>(&mut split)),
 
-            "/r" | "/rm" | "/remove" => Command::Remove(next_required::<String>(&mut split)),
+            "/r" | "/rm" | "/remove" => Command::Remove({
+                match (
+                    next_required::<String>(&mut split),
+                    next_optional::<String>(&mut split),
+                ) {
+                    (Ok(service_name), Ok(service_login)) => {
+                        Ok(Target::new(Option::from(service_name), service_login))
+                    }
+                    (Err(error), _) | (_, Err(error)) => Err(error),
+                }
+            }),
 
             "/a" | "/add" => Command::Add(parse_service::<String>(&mut split)),
 
             "/fg" | "/fastgen" => Command::FastGenerate(next_required::<usize>(&mut split)),
 
-            "/l" | "/list" => Command::List(match next_optional::<String>(&mut split) {
-                Ok(Some(service_name)) => SubCommand::ByService(service_name),
-                _ => SubCommand::Default,
+            "/l" | "/list" => Command::List({
+                match (
+                    next_optional::<String>(&mut split),
+                    next_optional::<String>(&mut split),
+                ) {
+                    (Ok(service_opt), Ok(service_login_opt)) => {
+                        Ok(Target::new(service_opt, service_login_opt))
+                    }
+                    (Err(_), _) | (_, Err(_)) => Ok(Target::new(None, None)),
+                }
             }),
 
             "/c" | "/clear" => Command::Clear,
