623bdec79fa534d9ac372c3ab205b9e6ba6cca03
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-06-06 23:13:47 +0300

    New: flag struct module

--- src/models/flag.rs
+++ src/models/flag.rs
@@ -1,13 +1,5 @@
-pub enum Flag {
-    Show,
-    Unknown(String),
-}
-
-impl From<&str> for Flag {
-    fn from(flag: &str) -> Self {
-        match flag{
-            "--show" => Flag::Show,
-            other => Flag::Unknown(other.to_string()),
-        }
-    }
+#[derive(Default)]
+pub struct Flags {
+    pub show: bool,
+    pub generate: usize,
 }
