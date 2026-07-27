300c3f5044d07662e1fe583b7db7b241b987c95b
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-07-27 20:24:12 +0300

    WIP launch

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,22 +1,9 @@
-#include <QApplication>
-#include <QLabel>
-#include <QVBoxLayout>
+#include <ui/runner.hpp>
 
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    QApplication app(argc, argv);
-    QWidget window;
-    window.setWindowTitle("Hello World!");
-
-    QVBoxLayout main_layout(&window);
-
-    QLabel label("Password-Manager");
-
-    label.setAlignment(Qt::AlignCenter);
-
-    main_layout.addWidget(&label);
-
-    window.show();
-    return QApplication::exec();
+    MainController mainController;
+    Runner runner{mainController};
+    runner.run(argc, argv);
 }
