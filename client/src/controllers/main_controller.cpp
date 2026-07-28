194b08054198febb342d26973ea4fe2b8b22f8df
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-07-28 23:27:50 +0300

    WIP: start writing main controller

--- client/src/controllers/main_controller.cpp
+++ client/src/controllers/main_controller.cpp
@@ -1,2 +1,8 @@
 #include <controllers/main_controller.hpp>
 
+MainController::MainController() : storageController_(StorageController{}) {}
+
+const std::vector<Service>& MainController::getServices()
+{
+    return storageController_.services();
+}
--- client/src/controllers/main_controller.hpp
+++ client/src/controllers/main_controller.hpp
@@ -1,18 +1,29 @@
 #pragma once
 #include <memory>
 
+#include "storage_controller.hpp"
+
 
 class MainController
 {
 
 public:
-    MainController() = default; // TODO TEMP
-    ~MainController() = default; // TODO TEMP
+    MainController();
+    ~MainController() = default;
     MainController(const MainController&) = delete;
     MainController(MainController&&) = delete;
     MainController& operator=(const MainController&) = delete;
     MainController& operator=(MainController&&) = delete;
 
+    const std::vector<Service>& getServices();
+
+
 
 private:
+    /*
+     * Before adding tests (if they will?)
+     * We can store original version of object
+     * we currently use :)
+    */
+    StorageController storageController_;
 };
