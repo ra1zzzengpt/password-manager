5ee334dfdd3b2f973e3f4d00dcc09362372065b8
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-07-30 23:25:32 +0300

    WIP: primitive generation added

--- client/src/generate/generator.cpp
+++ client/src/generate/generator.cpp
@@ -1,3 +1,19 @@
 #include "generator.hpp"
 
 #include <random>
+namespace {
+    const std::string ALLOWED_SYMBOLS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+}
+std::string Generator::generate_random_password(const uint32_t length)
+{
+    static std::mt19937_64 gen(std::random_device{}());
+    std::uniform_int_distribution<size_t> dis(0, ALLOWED_SYMBOLS.size() - 1);
+
+    std::string result;
+    result.reserve(length);
+    for (uint32_t i = 0; i < length; ++i)
+    {
+        result.push_back(ALLOWED_SYMBOLS[dis(gen)]);
+    }
+    return result;
+}
--- client/src/generate/generator.hpp
+++ client/src/generate/generator.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <string>
+#include <cstdint>
 // todo enum levels
 
 class Generator final
 {
+    public:
     // todo generator level for easy normal and hard password
+    static std::string generate_random_password(uint32_t length);
 };
