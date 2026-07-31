2c745c321d821bd6a8d886f796eea017c6080907
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-07-31 22:21:45 +0300

    WIP: Added connecting signal

--- client/src/ui/runner.cpp
+++ client/src/ui/runner.cpp
@@ -11,20 +11,28 @@ Runner::Runner(MainController &controller) : controller_(controller) {}
 void Runner::run(int argc, char *argv[])
 {
     QApplication app{argc, argv};
-    PWScreen pw_screen{controller_};
-    MainScreen main_screen{controller_};
-
     QStackedWidget *stack = new QStackedWidget;
+
+    PWScreen* pw_screen = new PWScreen(controller_, stack);
+    MainScreen* main_screen = new MainScreen(controller_, stack);
 
     QWidget* window = new QWidget;
     QVBoxLayout *mainLayout = new QVBoxLayout(window);
 
     window->setLayout(mainLayout);
 
-    stack->addWidget(pw_screen.build(window,stack));
-    stack->addWidget(main_screen.build(window,stack));
+    stack->addWidget(pw_screen);
+    stack->addWidget(main_screen);
+
+    QObject::connect(pw_screen, &PWScreen::unlocked, main_screen, &MainScreen::refresh);
+
+    QObject::connect(pw_screen, &PWScreen::unlocked, stack, [stack, main_screen]()
+    {
+        stack->setCurrentWidget(main_screen);
+    });
 
     mainLayout->addWidget(stack);
+
     window->show();
     QApplication::exec();
 }
