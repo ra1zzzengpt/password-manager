d1203ce648a7c9d04de81d3ace7e2438b3f9adec
ra1zzzengpt <ra1zzzengpt@gmail.com>
2026-09-19 13:20:24 +0300

    TODO: new field

--- client/src/domain/service.hpp
+++ client/src/domain/service.hpp
@@ -7,6 +7,7 @@ struct Service
     std::string name;
     std::string login;
     std::string password;
+    // todo std::string create_data;
 };
 
 void to_json(nlohmann::json& json, const Service& service);
